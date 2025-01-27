837e522951681bdace5225a17902545b91093636
cocoiro <107580843+cocoiro@users.noreply.github.com>
2025-01-26 23:12:57 -0800

    Add files via upload

---
+++ Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  SustainIFLY
+//
+//  Created by jiji lee!
+//
+
+import Foundation
+import SwiftData
+/*
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
+*/
